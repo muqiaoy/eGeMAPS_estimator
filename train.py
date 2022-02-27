f2061402c4c3ded718fed2e6bfa3665071fe4698
muqiaoy <muqiaoy@andrew.cmu.edu>
2022-02-27 16:26:17 -0500

    training of demucs

--- train.py
+++ train.py
@@ -5,16 +5,41 @@ import os
 import yaml
 import random
 import numpy as np
+from glob import glob
+from pathlib import Path
+import shutil
 
 import torch
 
 from model import *
 from data import distrib
 from data.dataset import NoisyCleanSet
+from trainer.trainer import Trainer
 
 
 def main(args):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)-8s %(message)s", filename="%s/train.log" % args.savePath, filemode='w')
+    if not os.path.exists(args.savePath):
+        os.makedirs(args.savePath)
+
+    # log rotation
+    logname = "%s/train.log" % args.savePath
+    max_num_log_files = 100
+    for i in range(max_num_log_files - 1, -1, -1):
+        if i == 0:
+            p = Path(logname)
+            pn = p.parent / (p.stem + ".1" + p.suffix)
+        else:
+            _p = Path(logname)
+            p = _p.parent / (_p.stem + f".{i}" + _p.suffix)
+            pn = _p.parent / (_p.stem + f".{i + 1}" + _p.suffix)
+
+        if p.exists():
+            if i == max_num_log_files - 1:
+                p.unlink()
+            else:
+                shutil.move(p, pn)
+
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)-8s (%(filename)s:%(lineno)d) %(message)s", filename=logname, filemode='w')
     if args.model == 'NSNet2':
         model = NSNet2(modelfile=args.modelPath, cfg=args.cfg)
         logging.info("Loaded checkpoint from %s" % args.modelPath)
@@ -31,12 +56,26 @@ def main(args):
         tr_dataset = NoisyCleanSet(tr_noisy_dir, tr_clean_dir, length=length, stride=stride, pad=args.pad, matching=args.matching, sample_rate=args.fs)
         tr_loader = distrib.loader(
             tr_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-        print("Total number of clean files: %s" % len(tr_dataset))
+        print("Total number of train frames: %s" % len(tr_dataset))
+        tt_noisy_dir = os.path.join(args.dataPath, "test_set/synthetic/no_reverb/noisy")
+        tt_clean_dir = os.path.join(args.dataPath, "test_set/synthetic/no_reverb/clean")
+        tt_dataset = NoisyCleanSet(tt_noisy_dir, tt_clean_dir, length=length, stride=stride, pad=args.pad, matching=args.matching, sample_rate=args.fs)
+        tt_loader = distrib.loader(
+            tt_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+        print("Total number of test frames: %s" % len(tt_dataset))
+
+        data = {"tr_loader": tr_loader, "cv_loader": None, "tt_loader": tt_loader}
+
+        if torch.cuda.is_available():
+            model.cuda()
 
         if args.optim == "Adam":
             optimizer = torch.optim.Adam(model.parameters(), lr=float(args.lr), betas=(0.9, args.beta2))
         else:
             raise NotImplementedError
+            
+        trainer = Trainer(data, model, optimizer, args)
+        trainer.train()
 
     else:
         raise NotImplementedError
